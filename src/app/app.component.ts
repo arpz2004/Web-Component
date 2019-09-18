4c6113f365998d5889bf665159b4480029a489cc
Adrien Lindner <lindner.54@osu.edu>
2019-09-17 22:59:15 -0400

    IE11 now works with shadow dom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,7 +5,7 @@ import { ViewEncapsulation } from '@angular/compiler/src/core';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  // encapsulation: ViewEncapsulation.ShadowDom
+  encapsulation: ViewEncapsulation.ShadowDom
 })
 export class AppComponent {
   title = 'web-components';
