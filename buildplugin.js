fba1272b899359b85140206b51b083c749be7332
Adrien Lindner <lindner.54@osu.edu>
2019-10-11 18:18:43 -0400

    Fixed typo

--- buildplugin.js
+++ buildplugin.js
@@ -2,6 +2,6 @@
 exports.__esModule = true;
 exports["default"] = {
   post: function() {
-    require('child_process').execSync('node node_modules/concat/bin/concat -o ./dist/web-components/web-components.js ./dist/web-components/runtime.js ./dist/web-components/polyfills.js ./dist/mapping-utility/scripts.js ./dist/mapping-utility.main.js')
+    require('child_process').execSync('node node_modules/concat/bin/concat -o ./dist/web-components/web-components.js ./dist/web-components/runtime.js ./dist/web-components/polyfills.js ./dist/web-components/scripts.js ./dist/web-components/main.js')
   }
 }
